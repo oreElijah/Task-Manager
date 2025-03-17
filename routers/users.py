a57ca263d9ec2147b2e93ec20668f157b033f156
oreElijah <151626649+oreElijah@users.noreply.github.com>
2025-03-17 21:14:14 +0100

    Update users.py

--- routers/users.py
+++ routers/users.py
@@ -23,7 +23,6 @@ async def register_user(user: UserIn):
 
 @router.post("/login", status_code=200)
 async def login_user(user: UserLogin):
-    print(user.id)
     cond = await get_user(user.email)
     if not cond:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with the email does not exist")
